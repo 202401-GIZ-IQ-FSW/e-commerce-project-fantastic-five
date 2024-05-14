387c3657aba11374a6669c5dfc386745cb637b88
elaf <elafghassangardi@gmail.com>
2024-05-14 20:31:54 +0300

    admin roles

--- server/models/shop-item.js
+++ server/models/shop-item.js
@@ -1,1 +1,42 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ShopItemSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true // Removes whitespace from both ends of a string
+    },
+    image: {
+        type: String, // Assuming URL of the image
+        required: true,
+        trim: true // Removes whitespace from both ends of a string
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0 // Ensure the price is a non-negative number
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true // Removes whitespace from both ends of a string
+    },
+    availableCount: {
+        type: Number,
+        required: true,
+        min: 0 // Ensure the available count is a non-negative number
+    },
+    genre: {
+        type: String,
+        required: true,
+        trim: true // Removes whitespace from both ends of a string
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now // Sets the default value to the current date and time
+    }
+});
+
+// Export the model
+module.exports = mongoose.model('ShopItem', ShopItemSchema);

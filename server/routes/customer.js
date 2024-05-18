b801af8452adc4de6d3a4814884b1c7c57ab70c1
Fawzi Gharib <fawz11gharib@gmail.com>
2024-05-18 23:27:30 +0300

    FIX: Removed user end point

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const ShopItem = require('../models/shop-item');
-
-
-
-
+const Cart = require('../models/user');
+const User = require('../models/user');
+const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 
 
 /// Getting all shop items with search functionality
@@ -25,7 +24,6 @@ router.get('/items', async (req, res) => {
   }
 })
 
-
 // Ensuring that min and max prices are valid
 function isValidPrice(price) {
   const regex = /^[0-9]+(\.[0-9]+)?$/;
@@ -34,6 +32,60 @@ function isValidPrice(price) {
 
 // -------------------------------
 
+
+
+
+/// Getting single shop item 
+router.get('/items/:id', async (req, res) => {
+  try {
+    const item = await ShopItem.findById(req.params.id); // Find the item by ID and update it with the new data from the request body
+    if (!item) return res.status(404).json({ error: 'Item not found' });
+    res.json(item); // Respond with the updated item
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.post('/cart', async (req, res) => {
+  try {
+    const { customerId, itemId, quantity } = req.body;
+
+    // Check if item exists in inventory
+    const item = await ShopItem.findById(itemId);
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+    // Check if requested quantity is available in inventory
+    if (item.availableCount < quantity) {
+      return res.status(400).json({ message: 'Insufficient quantity' });
+    }
+
+    // Decrement quantity in inventory
+    item.availableCount -= quantity;
+    await item.save();
+
+    const userObject = new User();
+    userObject.cart.push(item);
+
+    res.status(200).json({ message: 'Item added to cart' },);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.get('/cart', ensureAuthenticated, async (req, res) => {
+  try {
+  
+    console.log(user);
+    const cart = await Cart.find();
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 
 
